061cf67cc14c676b4fc64137cfa38ace702ac74f
Index Int <vlad.z.4096@gmail.com>
2016-03-21 14:12:44 +0300

    Benchmark more labels

--- bench/Benchmark.hs
+++ bench/Benchmark.hs
@@ -14,26 +14,34 @@ union3 :: OpenUnion '[Proxy 0, Proxy 1, ()]
 union3 = openUnion # ()
 {-# NOINLINE union3 #-}
 
-type OpenUnion7 = OpenUnion
+type OpenUnion12 = OpenUnion
   '[ Proxy 0, Proxy 1, Proxy 2, Proxy 3
-   , Proxy 4 , Proxy 5, Proxy 6, () ]
+   , Proxy 4, Proxy 5, Proxy 6, Proxy 7
+   , Proxy 8, Proxy 9, Proxy 10, () ]
 
-union7 :: OpenUnion7
-union7 = openUnion # ()
-{-# NOINLINE union7 #-}
+union12 :: OpenUnion12
+union12 = openUnion # ()
+{-# NOINLINE union12 #-}
 
-either7 ::
-  Either (Proxy 0) (
-  Either (Proxy 1) (
-  Either (Proxy 2) (
-  Either (Proxy 3) (
-  Either (Proxy 4) (
-  Either (Proxy 5) (
-  Either (Proxy 6) (
+either12 ::
+  Either (Proxy 0)  (
+  Either (Proxy 1)  (
+  Either (Proxy 2)  (
+  Either (Proxy 3)  (
+  Either (Proxy 4)  (
+  Either (Proxy 5)  (
+  Either (Proxy 6)  (
+  Either (Proxy 7)  (
+  Either (Proxy 8)  (
+  Either (Proxy 9)  (
+  Either (Proxy 10) (
       ()
-  )))))))
-either7 = (Right . Right . Right . Right . Right . Right . Right) ()
-{-# NOINLINE either7 #-}
+  )))))))))))
+either12 =
+  ( Right . Right . Right . Right
+  . Right . Right . Right . Right
+  . Right . Right . Right ) ()
+{-# NOINLINE either12 #-}
 
 main :: IO ()
 main = do
@@ -42,14 +50,14 @@ main = do
         whnf (\a -> a ^? openUnion :: Maybe ()) union1
     , bench "openUnion matching 3rd" $
         whnf (\a -> a ^? openUnion :: Maybe ()) union3
-    , bench "openUnion matching 7th" $
-        whnf (\a -> a ^? openUnion :: Maybe ()) union7
-    , bench "nested either matching 7th" $
-        whnf (\a -> a ^? _Right . _Right . _Right
-                       . _Right . _Right . _Right
-                       . _Right :: Maybe ()) either7
-    , bench "openUnion constructing 7th" $
-        whnf (\a -> openUnion # a :: OpenUnion7) ()
+    , bench "openUnion matching 12th" $
+        whnf (\a -> a ^? openUnion :: Maybe ()) union12
+    , bench "nested either matching 12th" $
+        whnf (\a -> a ^? _Right . _Right . _Right . _Right
+                       . _Right . _Right . _Right . _Right
+                       . _Right . _Right . _Right :: Maybe ()) either12
+    , bench "openUnion constructing 12th" $
+        whnf (\a -> openUnion # a :: OpenUnion12) ()
     , bench "dyn constructing" $
         whnf toDyn ()
     ]
